3c5d63fac1f8b74d57cf1a139873c0c10d2e2b45
gusbrigantino <gubrig97@gmail.com>
2021-04-25 20:35:03 -0700

    timelapse1

--- image_capture.py
+++ image_capture.py
@@ -10,18 +10,20 @@ path = os.getcwd()
 folder = ''
 img_ext = '.jpg'
 
-stop_time = datetime.datetime(2021, 4, 24, 20, 0, 0)
+stop_time = datetime.datetime(2021, 4, 25, 20, 30, 0)
 
 folder = '/' + str(stop_time.date())
+
+os.mkdir(path + folder)
 
 i = 0
 while True:
     name = '/' + str(i).zfill(4)
-    camera.capture(path + name + img_ext)
+    camera.capture(path + folder + name + img_ext)
     curr_time = datetime.datetime.now()
     if curr_time < stop_time:
         i += 1
-        sleep(3)
+        sleep(180)
     else:
         exit()
     
